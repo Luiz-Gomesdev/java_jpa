abf666a35a43140ded33ed717e0bcc8d6b7a3b7f
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-04-29 12:53:36 -0300

    ending program

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,20 +1,31 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Lucio Mauro Filho", "lucio@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Carlos Almeida Maciel", "carlos@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Luana Vieira Assis", "luana@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Lucio Mauro Filho", "lucio@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Carlos Almeida Maciel", "carlos@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Luana Vieira Assis", "luana@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
 		
-
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+		
 	}
 
 }
